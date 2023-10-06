84f36a6bd7335d4e652b5123b25324d874232255
Mohit Adhikari <143146421+mohitdebugs@users.noreply.github.com>
2023-10-06 20:42:30 +0530

    Create 06-10-2023 | Reverse Alternative Node in Linked List.cpp

---
+++ 10 October-2023/06-10-2023 | Reveerse Alternative Node in Linked List.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    public:
+    
+    void rearrange(struct Node *odd)
+    {
+        Node*first = odd;
+        Node*second = odd->next;
+        Node*temp = odd->next;
+        while(first->next and second->next) {
+            first->next = first->next->next;
+            first = first->next;
+            second->next = second->next->next;
+            second = second->next;
+        }
+        first->next = reverse(temp);
+    }
+    Node *reverse(Node*head) {
+        if(!head or !head->next) {
+            return head;
+        }
+        Node*smallHead = reverse(head->next);
+        Node*temp = head;
+        temp->next->next = head;
+        temp->next = NULL;
+        return smallHead;
+    }
+};

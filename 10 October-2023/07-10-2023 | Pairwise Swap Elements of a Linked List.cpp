b35681fbd3acce21fc3433abcb14c1a2be858ef9
Mohit Adhikari <143146421+mohitdebugs@users.noreply.github.com>
2023-10-07 20:08:04 +0530

    Create 07-10-2023 | Pairwise Swap Elements of a Linked List.cpp

---
+++ 10 October-2023/07-10-2023 | Pairwise Swap Elements of a Linked List.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+    public:
+    Node* pairWiseSwap(struct Node* head) 
+    {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+        Node* prev = head;
+        Node* curr = head->next;
+        head = curr;
+        while (1) {
+            Node* next = curr->next;
+            curr->next = prev;
+            if (next == nullptr || next->next == nullptr) {
+                prev->next = next;
+                break;
+            } else {
+                prev->next = next->next;
+            }
+
+            prev = next;
+            curr = next->next;
+        }
+
+        return head;
+    }
+};
